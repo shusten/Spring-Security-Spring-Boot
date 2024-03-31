6cbf4b1d4c420770eebefdb794e1e06e2d7a3fcb
Luis Villas-Boas <shusten@hotmail.com>
2024-03-30 22:57:40 -0300

    :tada: Begin a project

---
+++ src/test/java/covra/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package covra.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
