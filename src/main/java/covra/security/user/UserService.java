0c8e66f659f8ce98dfa3f41484b14dfe07a865b0
Luis Villas-Boas <shusten@hotmail.com>
2024-03-31 19:42:56 -0300

    Add UserService

---
+++ src/main/java/covra/security/user/UserService.java
@@ -0,0 +1,5 @@
+package covra.security.user;
+
+public interface UserService {
+    User create(User user);
+}
---
+++ src/main/java/covra/security/user/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package covra.security.user;
+
+public class UserServiceImpl implements UserService{
+    @Override
+    public User create(User user) {
+        return null;
+    }
+}
