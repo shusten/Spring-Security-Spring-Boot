b5bb5a3981d48833bec246789a91c60f403e5996
Luis Villas-Boas <shusten@hotmail.com>
2024-03-31 02:17:14 -0300

    Add Role class

---
+++ src/main/java/covra/security/role/Role.java
@@ -0,0 +1,21 @@
+package covra.security.role;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ROLE")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
