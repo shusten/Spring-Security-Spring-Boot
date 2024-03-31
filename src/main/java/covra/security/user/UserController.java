ecc1262985348b4dc12507f1c85fd314a69d40e6
Luis Villas-Boas <shusten@hotmail.com>
2024-03-31 19:37:59 -0300

    Add UserController

---
+++ src/main/java/covra/security/user/UserController.java
@@ -0,0 +1,9 @@
+package covra.security.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+}
