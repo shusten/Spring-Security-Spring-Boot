a214fc9c9a4bbbb8d3faf065757c55448b44a8e3
Luis Villas-Boas <shusten@hotmail.com>
2024-04-04 23:08:14 -0300

    Add crud

---
+++ src/main/java/covra/security/product/ProductService.java
@@ -0,0 +1,10 @@
+package covra.security.product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> listAll();
+    Product create(Product product);
+    Product update(Product product);
+    void delete(Long id);
+}
